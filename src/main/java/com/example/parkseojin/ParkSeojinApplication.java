88a98af00c79511b94e545b63ec2787382482460
청매 <tjwls0026@gmail.com>
2026-06-16 22:19:40 +0900

    chore :: project setting

---
+++ src/main/java/com/example/parkseojin/ParkSeojinApplication.java
@@ -0,0 +1,13 @@
+package com.example.parkseojin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkSeojinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkSeojinApplication.class, args);
+    }
+
+}
